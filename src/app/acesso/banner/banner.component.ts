987e2bc7f56aa02df38a87e61db98c6d8eae0649
V-Icaro <venicius.icarosr@gmail.com>
2020-02-20 00:00:30 -0300

    Iniciando animações no componente banner

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {  trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      }))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'escondido'
 
   constructor() { }
 
