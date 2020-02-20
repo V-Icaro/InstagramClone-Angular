a34ce409398aa8df05717bce85317b6e7c418f90
V-Icaro <venicius.icarosr@gmail.com>
2020-02-20 00:33:03 -0300

    Criando model para imagens do banner

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {  trigger, state, style, transition, animate } from '@angular/animations';
 
+import { Imagem } from './imagem.model'
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
@@ -21,9 +23,18 @@ export class BannerComponent implements OnInit {
 
   public estado: string = 'escondido'
 
+  public imagens: Imagem[] = [
+    { estado:'escondido', url:'/assets/banner-acesso/img_1.png'},
+    { estado:'escondido', url:'/assets/banner-acesso/img_2.png'},
+    { estado:'escondido', url:'/assets/banner-acesso/img_3.png'},
+    { estado:'escondido', url:'/assets/banner-acesso/img_4.png'},
+    { estado:'escondido', url:'/assets/banner-acesso/img_5.png'}
+  ]
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.imagens)
   }
 
   public toggleEstado(): void {
---
+++ src/app/acesso/banner/imagem.model.ts
@@ -0,0 +1,8 @@
+class Imagem {
+    constructor(
+        public estado: string,
+        public url: string
+    ) {}
+}
+
+export { Imagem }
