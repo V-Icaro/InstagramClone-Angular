c4946af46b952a56c4875adc18df776d16650053
V-Icaro <venicius.icarosr@gmail.com>
2020-02-20 22:38:58 -0300

    Estilos na transição do banner

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, animate, style, state } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(0, -50px)'
+        }),
+        animate('500ms 0s ease-in-out') //duraçao, delay e aceleracao
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string = 'criado'
 
   constructor() { }
 
