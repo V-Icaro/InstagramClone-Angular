de83307a77e1044c9853e50d9a96b4c40d5e27ee
V-Icaro <venicius.icarosr@gmail.com>
2020-02-24 08:58:20 -0300

    Conectando com Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app3';
+
+  ngOnInit(): void{
+    let firebaseConfig = {
+      apiKey: "AIzaSyAI580oBd4sG10lQXp8tbm99A0_EeXyOf4",
+      authDomain: "instagramclone-angular.firebaseapp.com",
+      databaseURL: "https://instagramclone-angular.firebaseio.com",
+      projectId: "instagramclone-angular",
+      storageBucket: "instagramclone-angular.appspot.com",
+      messagingSenderId: "771784086036",
+      appId: "1:771784086036:web:3ebd958a814190565bcd01"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
