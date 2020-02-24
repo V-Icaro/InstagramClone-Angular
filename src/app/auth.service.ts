af60b240b525169921076b05eb906b3b11820641
V-Icaro <venicius.icarosr@gmail.com>
2020-02-24 09:26:01 -0300

    Cadastrando usuarios no firebase

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,16 @@
 import { Usuario } from './acesso/usuario.model'
+import * as firebase from 'firebase'
 
 export class Auth {
     public cadastrarUsuario(usuario: Usuario): void {
         console.log('Chegamos até o serviço: ' , usuario)
+
+        firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
+            .then((resposta: any) => {
+                console.log(resposta)
+            })
+            .catch((error: Error) => {
+                console.log(error)
+            })
     }
 }
