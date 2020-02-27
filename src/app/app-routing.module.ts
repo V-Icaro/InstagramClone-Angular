5b896a4b0678391ac7c757e2c2c1a56e3e8f75f6
V-Icaro <venicius.icarosr@gmail.com>
2020-02-26 21:26:12 -0300

    Configurando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AcessoComponent } from './acesso/acesso.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AcessoComponent},
+  { path: 'home', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
